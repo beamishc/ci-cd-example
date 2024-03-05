76f83e69faf260d0bdab11554ef8b67ba49c1e0f
Catriona Beamish <catriona.beamish@gmail.com>
2024-03-05 00:37:07 +0000

    Update app.py
    add an image

--- app.py
+++ app.py
@@ -7,3 +7,5 @@ st.write(root())
 st.write('This is obviously very simple, in reality one would rather use this page to display API results more nicely! :)')
 
 st.write('I am adding a change to show cd')
+
+st.image('https://github.com/beamishc/ci-cd-example/blob/ebd95ed5f0110f93c9ed17f1b223a9268edf2ccb/Screenshot_20221117-112100.png')
