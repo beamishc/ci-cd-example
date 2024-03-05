8c6c6d469b1050cb1bde178c84ef9add16d1df33
Catriona Beamish <catriona.beamish@gmail.com>
2024-03-05 00:39:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ st.write('This is obviously very simple, in reality one would rather use this pa
 
 st.write('I am adding a change to show cd')
 
-st.image('https://github.com/beamishc/ci-cd-example/blob/ebd95ed5f0110f93c9ed17f1b223a9268edf2ccb/Screenshot_20221117-112100.png')
+st.image('Screenshot_20221117-112100.png')
